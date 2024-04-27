eeaf9ad530e53e2cf141db5741b6c6dbce9038cd
Gerard Comas <gerard.comas@estudiantat.upc.edu>
2024-04-27 12:09:17 +0200

    Redistribución de ficheros

--- game/Board.py
+++ game/Board.py
@@ -1,6 +1,27 @@
 import random
 from GameConstants import *
 
+class Hexagon:
+
+  def __init__(self, resource, number, id):
+    self.id = id
+    self.resource = resource  
+    self.number = number
+    self.tiles = []
+
+  def addTiles(self, tiles):
+    for tile in tiles:
+      self.tiles.append(tile)
+
+  def __repr__(self):
+    tiles = self.tiles
+    val = "--------------HEXAGON INFO---------------\n"
+    val += "Hexagon: " + str(self.id) +"\n"
+    val += "Resource Type: " + str(self.resource) +"\n"
+    val += "Tile number: " + str(self.number) +"\n"
+    return val
+
+
 class Tile:
   """
   Class: Tile
@@ -12,13 +33,12 @@ class Tile:
   """
 
 
-  def __init__(self, resource, number, x, y):
-    self.resource = resource
+  def __init__(self, x, y):
     self.x = x
     self.y = y
-    self.number = number
     self.player = None
     self.structure = Structure["NONE"] # Settlement, vertical road, or horizontal road
+    self.hexagonids = []
 
 
   """
@@ -48,9 +68,12 @@ class Tile:
   def settle(self, playerIndex):
     if self.isOccupied():
       raise Exception("This tile is already used!")
+    if self.isWater(): 
+      raise Exception("This tile is water!")
     self.player = playerIndex
     self.structure = Structure["SETTLEMENT"]
   
+
   """
   Method: upgrade
   ---------------------------
@@ -85,8 +108,14 @@ class Tile:
   def buildRoad(self, playerIndex):
     if self.isOccupied():
       raise Exception("Tile " + str(self) + " is already used!")
+    if self.isWater(): 
+      raise Exception("This tile is water!")
     self.player = playerIndex
     self.structure = Structure["ROAD"]
+
+  
+  def addHexagon(self, hexagon):
+    self.hexagonids.append(hexagon.id)
 
 
   """
@@ -102,9 +131,12 @@ class Tile:
     val = "--------------TILE INFO AT (" + str(self.x) + ", " + str(self.y) + ")---------------\n"
     val += "Owned by player: " + str(self.player) +"\n"
     val += "Structure: " + str(self.structure) +"\n"
-    val += "Resource Type: " + str(self.resource) +"\n"
-    val += "Tile number: " + str(self.number) +"\n"
     return val
+  
+
+  def isWater(self):
+    if (self.x == 0 and self.y == 0) or (self.x == 0 and self.y == 1) or (self.x == 1 and self.y == 0) or (self.x == 4 and self.y == 0) or (self.x == 5 and self.y == 1) or (self.x == 5 and self.y == 0) or (self.x == 0 and self.y == 9) or (self.x == 0 and self.y == 10) or (self.x == 1 and self.y == 10) or (self.x == 4 and self.y == 10) or (self.x == 5 and self.y == 9) or (self.x == 5 and self.y == 10): return True
+    return False
 
 
   """
@@ -121,13 +153,18 @@ class Tile:
   ---------------------------
   """
   def strRepresentation(self):
+    if self.isWater(): return "XX"
     if not self.isOccupied(): return "--"
-    elif self.structure == Structure["ROAD"]: return "R" + str(self.player)
-    elif self.structure == Structure["SETTLEMENT"]: return "S" + str(self.player)
+    if self.structure == Structure["ROAD"]:
+        return COLORS[self.player] + "R" + str(self.player) + END_COLOR
+    if self.structure == Structure["SETTLEMENT"]:
+        return COLORS[self.player]  + "S" + str(self.player) + END_COLOR
+    if self.structure == Structure["CITY"]:
+        return COLORS[self.player]  + "C" + str(self.player) + END_COLOR
     raise Exception("strRepresentation - invalid tile")
   
 
-class BasicBoard:
+class Board:
   """
   Class: BasicBoard
   ---------------------------
@@ -139,25 +176,62 @@ class BasicBoard:
   ---------------------------
   """
 
-  def __init__(self, size=5):
-    self.board = []
+  def __init__(self, size_x=6, size_y=11):
 
-    possibleResources = [] 
-    for resource in ResourceTypes.values():
-      possibleResources += 5*[resource]
-      random.shuffle(possibleResources)
+    self.size_x = size_x
+    self.size_y = size_y
 
-    possiblenumbers = [2,2,3,3,4,4,5,5,5,6,6,6,8,8,8,9,9,9,10,10,10,11,11,12,12]
+    possibleResources = [4,4,4,4,1,1,1,1,3,3,3,3,2,2,2,0,0,0] 
+    random.shuffle(possibleResources)
+
+    possiblenumbers = [2,3,3,4,4,5,5,5,6,6,8,8,9,9,10,10,11,11,12]
     random.shuffle(possiblenumbers)
-    for i in range(size):
+
+    self.hexagons = []
+
+    for i in range (19):
+      if i == 9:
+        hexagon = Hexagon(-1, 7, 9)
+      elif i == 18:
+        hexagon = Hexagon(possibleResources[9], possiblenumbers[9], 19)
+      else:
+        hexagon = Hexagon(possibleResources[i], possiblenumbers[i], i)
+      self.hexagons.append(hexagon)
+
+    
+    self.hexagons[0].addTiles([Tile(0, 2), Tile(0, 3), Tile(0, 4), Tile(1, 2), Tile(1, 3), Tile(1, 4)])
+    self.hexagons[1].addTiles([Tile(0, 4), Tile(0, 5), Tile(0, 6), Tile(1, 4), Tile(1, 5), Tile(1, 6)]) 
+    self.hexagons[2].addTiles([Tile(0, 6), Tile(0, 7), Tile(0, 8), Tile(1, 6), Tile(1, 7), Tile(1, 8)])
+    self.hexagons[3].addTiles([Tile(1, 1), Tile(1, 2), Tile(1, 3), Tile(2, 1), Tile(2, 2), Tile(2, 3)])
+    self.hexagons[4].addTiles([Tile(1, 3), Tile(1, 4), Tile(1, 5), Tile(2, 3), Tile(2, 4), Tile(2, 5)])
+    self.hexagons[5].addTiles([Tile(1, 5), Tile(1, 6), Tile(1, 7), Tile(2, 5), Tile(2, 6), Tile(2, 7)])
+    self.hexagons[6].addTiles([Tile(1, 7), Tile(1, 8), Tile(1, 9), Tile(2, 7), Tile(2, 8), Tile(2, 9)])
+    self.hexagons[7].addTiles([Tile(2, 0), Tile(2, 1), Tile(2, 2), Tile(3, 0), Tile(3, 1), Tile(3, 2)])
+    self.hexagons[8].addTiles([Tile(2, 2), Tile(2, 3), Tile(2, 4), Tile(3, 2), Tile(3, 3), Tile(3, 4)])
+    self.hexagons[9].addTiles([Tile(2, 4), Tile(2, 5), Tile(2, 6), Tile(3, 4), Tile(3, 5), Tile(3, 6)])
+    self.hexagons[10].addTiles([Tile(2, 6), Tile(2, 7), Tile(2, 8), Tile(3, 6), Tile(3, 7), Tile(3, 8)])
+    self.hexagons[11].addTiles([Tile(2, 8), Tile(2, 9), Tile(2, 10), Tile(3, 8), Tile(3, 9), Tile(3, 10)])
+    self.hexagons[12].addTiles([Tile(3, 1), Tile(3, 2), Tile(3, 3), Tile(4, 1), Tile(4, 2), Tile(4, 3)])
+    self.hexagons[13].addTiles([Tile(3, 3), Tile(3, 4), Tile(3, 5), Tile(4, 3), Tile(4, 4), Tile(4, 5)])
+    self.hexagons[14].addTiles([Tile(3, 5), Tile(3, 6), Tile(3, 7), Tile(4, 5), Tile(4, 6), Tile(4, 7)])
+    self.hexagons[15].addTiles([Tile(3, 7), Tile(3, 8), Tile(3, 9), Tile(4, 7), Tile(4, 8), Tile(4, 9)])
+    self.hexagons[16].addTiles([Tile(4, 2), Tile(4, 3), Tile(4, 4), Tile(5, 2), Tile(5, 3), Tile(5, 4)])
+    self.hexagons[17].addTiles([Tile(4, 4), Tile(4, 5), Tile(4, 6), Tile(5, 4), Tile(5, 5), Tile(5, 6)])
+    self.hexagons[18].addTiles([Tile(4, 6), Tile(4, 7), Tile(4, 8), Tile(5, 6), Tile(5, 7), Tile(5, 8)])
+
+    self.board = []
+    for i in range(size_x):
       boardRow = []
-      for j in range(size):
-        boardRow.append(Tile(possibleResources.pop(), possiblenumbers.pop(), i, j))
+      for j in range(size_y):
+          boardRow.append(Tile(i, j))
       self.board.append(boardRow)
+
+    for i in range(19):
+      for tile in self.hexagons[i].tiles:
+        self.getTile(tile.x, tile.y).addHexagon(self.hexagons[i])
 
     self.settlements = []
     self.roads = []
-    self.size = size
 
 
   """
@@ -173,7 +247,7 @@ class BasicBoard:
   ---------------------------
   """
   def getTile(self, x, y):
-    if 0 <= x < self.size and 0 <= y < self.size:
+    if 0 <= x < self.size_x and 0 <= y < self.size_y:
       return self.board[x][y]
     return None
 
@@ -191,18 +265,69 @@ class BasicBoard:
   ---------------------------
   """
   def printBoard(self):
-    # Print top numbers
-    s = "     "
-    for i in range(self.size):
-      s += str(i) + "   "
-    print(s)
+    s = "                " + self.board[0][3].strRepresentation() + "        " + self.board[0][5].strRepresentation()  + "        " + self.board[0][7].strRepresentation() + "                   \n" 
+    s += "           " + self.board[0][2].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[0].resource) + " " + self.board[0][4].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[1].resource) + " " + self.board[0][6].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[2].resource)  + " " + self.board[0][8].strRepresentation() + " \n"
+    
+    s += "           " + self.board[1][2].strRepresentation() + "   " + str(self.hexagons[0].number) + "   " 
+    if self.hexagons[0].number < 10: s += " "
+    s += self.board[1][4].strRepresentation() + "   " + str(self.hexagons[1].number) + "   " 
+    if self.hexagons[1].number < 10: s += " "
+    s += self.board[1][6].strRepresentation() + "   " + str(self.hexagons[2].number)  + "   " 
+    if self.hexagons[2].number < 10: s += " "
+    s += self.board[1][8].strRepresentation() + " \n"
 
-    for i, row in enumerate(self.board):
-      s = str(i) + " ["
-      for tile in row:
-        s += " " + tile.strRepresentation() + " "
-      print(s + "]")
+    s += "      " + self.board[1][1].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[3].resource) + " " + self.board[1][3].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[4].resource) + " " + self.board[1][5].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[5].resource)  + " " + self.board[1][7].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[6].resource) + " " + self.board[1][9].strRepresentation() + " \n"
 
+    s += "      " + self.board[2][1].strRepresentation() + "   " + str(self.hexagons[3].number) + "   " 
+    if self.hexagons[3].number < 10: s += " "
+    s += self.board[2][3].strRepresentation() + "   " + str(self.hexagons[4].number) + "   " 
+    if self.hexagons[4].number < 10: s += " "
+    s += self.board[2][5].strRepresentation() + "   " + str(self.hexagons[5].number)  + "   " 
+    if self.hexagons[5].number < 10: s += " "
+    s += self.board[2][7].strRepresentation() + "   " + str(self.hexagons[6].number)  + "   " 
+    if self.hexagons[6].number < 10: s += " "
+    s += self.board[2][9].strRepresentation() +  " \n"
+  
+    s += " " + self.board[2][0].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[7].resource) + " " + self.board[2][2].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[8].resource) + " " + self.board[2][4].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[9].resource) + " " + self.board[2][6].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[10].resource) + " " + self.board[2][8].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[11].resource)  + " " + self.board[2][10].strRepresentation() + " \n"
+
+    s += " " + self.board[3][0].strRepresentation() + "   " + str(self.hexagons[7].number) + "   " 
+    if self.hexagons[7].number < 10: s += " "
+    s += self.board[3][2].strRepresentation() + "   " + str(self.hexagons[8].number) + "   " 
+    if self.hexagons[8].number < 10: s += " "
+    s += self.board[3][4].strRepresentation() + "   " + str(self.hexagons[9].number)  + "   " 
+    if self.hexagons[9].number < 10: s += " "
+    s += self.board[3][6].strRepresentation() + "   " + str(self.hexagons[10].number)  + "   " 
+    if self.hexagons[10].number < 10: s += " "
+    s += self.board[3][8].strRepresentation() + "   " + str(self.hexagons[11].number)  + "   " 
+    if self.hexagons[11].number < 10: s += " "
+    s += self.board[3][10].strRepresentation() + " \n"
+
+    s += "      " + self.board[3][1].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[12].resource) + " " + self.board[3][3].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[13].resource) + " " + self.board[3][5].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[14].resource) + " " + self.board[3][7].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[15].resource) + " " + self.board[3][9].strRepresentation() + " \n"
+
+    s += "      " + self.board[4][1].strRepresentation() + "   " + str(self.hexagons[12].number) + "   " 
+    if self.hexagons[12].number < 10: s += " "
+    s += self.board[4][3].strRepresentation() + "   " + str(self.hexagons[13].number) + "   " 
+    if self.hexagons[13].number < 10: s += " "
+    s += self.board[4][5].strRepresentation() + "   " + str(self.hexagons[14].number)  + "   " 
+    if self.hexagons[14].number < 10: s += " "
+    s += self.board[4][7].strRepresentation() + "   " + str(self.hexagons[15].number)  + "   " 
+    if self.hexagons[15].number < 10: s += " "
+    s += self.board[4][9].strRepresentation() + " \n"
+
+    s += "           " + self.board[4][2].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[16].resource) + " " + self.board[4][4].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[17].resource) + " " + self.board[4][6].strRepresentation() + " " + value2key(ResourceTypes, self.hexagons[18].resource) + " " + self.board[4][8].strRepresentation() +  " \n"
+
+    s += "           " + self.board[5][2].strRepresentation() + "   " + str(self.hexagons[16].number) + "   " 
+    if self.hexagons[16].number < 10: s += " "
+    s += self.board[5][4].strRepresentation() + "   " + str(self.hexagons[17].number) + "   " 
+    if self.hexagons[17].number < 10: s += " "
+    s += self.board[5][6].strRepresentation() + "   " + str(self.hexagons[18].number)  + "   " 
+    if self.hexagons[18].number < 10: s += " "
+    s += self.board[5][8].strRepresentation() +  " \n"
+    
+
+    s += "                " + self.board[5][3].strRepresentation() + "        " + self.board[5][5].strRepresentation()  + "        " + self.board[5][7].strRepresentation() + " \n" 
+    return print(s)
+  
 
   """
   Method: applyAction
@@ -300,11 +425,14 @@ class BasicBoard:
     resources = Counter() 
     
     for settlement in self.settlements:
-      if settlement.number == dieRoll and settlement.player == playerIndex:
-        resources[settlement.resource] += 1
-
-        if settlement.structure == Structure["CITY"]:
-            resources[settlement.resource] += 1
+      for hexagonid in settlement.hexagonids:
+        hexagon = self.hexagons[hexagonid]
+        if hexagon.number == dieRoll and settlement.player == playerIndex:
+          for resource in settlement.resources:
+            if settlement.structure == Structure["CITY"]:
+              resources[resource] += 2
+            else:
+              resources[resource] += 1
                 
     return resources
 
@@ -346,7 +474,10 @@ class BasicBoard:
     for neighbor in occupiedNeighbors:
       if neighbor.structure == Structure["SETTLEMENT"]:
         return False
-
+      occupiedNeighbors2 = self.getOccupiedNeighbors(neighbor, diagonals=False)
+      for neighbor2 in occupiedNeighbors2:
+        if neighbor2.structure == Structure["SETTLEMENT"]:
+          return False
     return True
 
 
--- game/Game.py
+++ game/Game.py
@@ -1,4 +1,4 @@
-from BoardCopy import *
+from Board import *
 from GameConstants import *
 from Agents import *
 
